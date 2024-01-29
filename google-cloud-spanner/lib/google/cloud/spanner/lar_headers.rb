18e63e5291e7c67a82e9886f6d89e8b3b037be23
Sandeep Tuniki <sandeep.tnk29@gmail.com>
2024-01-29 15:57:49 +0530

    Document LARHeaders module

--- google-cloud-spanner/lib/google/cloud/spanner/lar_headers.rb
+++ google-cloud-spanner/lib/google/cloud/spanner/lar_headers.rb
@@ -18,19 +18,42 @@ module Google
     module Spanner
       ##
       # @private Helper module to access header values
-      # for Leader Aware Routing
+      # for Leader Aware Routing (LAR).
+      #
+      # For a multi-region Cloud Spanner instance, the majority of
+      # the write latency is attributed to the network round trip
+      # caused by several RPCs between SpanFE and SpanServer. This
+      # latency is amplified when the client application is deployed
+      # away from the leader region.
+      #
+      # To decrease the latency, we use LAR to help Google Front End (GFE)
+      # or Cloudpath Front End (CFE) to route requests directly to the
+      # leader region or the nearest non-leader region.
+      #
+      # For every RPC, we pass the header `x-goog-spanner-route-to-leader`,
+      # whose value is either 'true' or 'false'. Some RPCs (such as admin APIs)
+      # don't pass the header since they're deprecated in the handwritten
+      # client. Also, users of the client can choose to opt out of the
+      # LAR feature, in which case, we don't pass the header.
+      #
+      # This module lists the header values for different RPCs to be used
+      # by the client. These values are expected to be constant. Some RPCs
+      # (such as `execute_streaming_sql`) can have different values
+      # depending on whether they're run within read-only transactions
+      # or read-write transactions.
+      #
       module LARHeaders
         module ClassMethods
-          def begin_transaction is_read_write
-            is_read_write ? "true" : "false"
+          def begin_transaction is_read_write_tx
+            is_read_write_tx ? "true" : "false"
           end
 
-          def read is_read_write
-            is_read_write ? "true" : "false"
+          def read is_read_write_tx
+            is_read_write_tx ? "true" : "false"
           end
 
-          def execute_query is_read_write
-            is_read_write ? "true" : "false"
+          def execute_query is_read_write_tx
+            is_read_write_tx ? "true" : "false"
           end
 
           def commit
