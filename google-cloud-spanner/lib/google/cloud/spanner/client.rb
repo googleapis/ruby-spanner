980069dfe98e629b6da91f868a7093d64f6357cd
hectorhammett <hecmenjac@google.com>
2025-09-09 17:51:45 +0000

    Update parameter documentation style

--- google-cloud-spanner/lib/google/cloud/spanner/client.rb
+++ google-cloud-spanner/lib/google/cloud/spanner/client.rb
@@ -878,10 +878,12 @@ module Google
         #      and all replicas are exhausted without finding a healthy replica,
         #      Spanner will wait for a replica in the list to become available,
         #      requests may fail due to DEADLINE_EXCEEDED errors.
-        # @param [::Google::Cloud::Spanner::V1::ReadRequest::OrderBy] order_by An option to control the order in which rows are returned from a read.
+        # @param [::Google::Cloud::Spanner::V1::ReadRequest::OrderBy] order_by An option to control the order in which
+        #   rows are returned from a read.
         #   To see the available options refer to
         #   [Google::Cloud::Spanner::V1::ReadRequest::OrderBy](https://cloud.google.com/ruby/docs/reference/google-cloud-spanner-v1/latest/Google-Cloud-Spanner-V1-ReadRequest-OrderBy)
-        # @param [::Google::Cloud::Spanner::V1::ReadRequest::LockHint lock_hint A lock hint mechanism for reads done within a transaction.
+        # @param [::Google::Cloud::Spanner::V1::ReadRequest::LockHint lock_hint A lock hint mechanism for reads done
+        #   within a transaction.
         #   To see the available options refer to
         #   [Google::Cloud::Spanner::V1::ReadRequest::LockHint](https://cloud.google.com/ruby/docs/reference/google-cloud-spanner-v1/latest/Google-Cloud-Spanner-V1-ReadRequest-LockHint)
         #
